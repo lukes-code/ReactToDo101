247fac59fc8b070d89a1938af3f70fa49dc310dc
lukes.code <lukesterry@gmail.com>
2019-11-12 07:56:01 +0000

    checkbox styling

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -15,11 +15,16 @@ export class TodoItem extends Component{
         return (
             <div style={items}>
                 <div style={this.getStyle()} className="todoBox">
-                    <label className="check">
-                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} id={this, id} /> {' '}
-                    { title }
-                    </label>
-                    <span className="checkmark"></span>
+                <label class="toggleButton">
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} id={this, id} /> 
+                    <div>
+                    <svg viewBox="0 0 44 44">
+                        <path d="M14,24 L21,31 L39.7428882,11.5937758 C35.2809627,6.53125861 30.0333333,4 24,4 C12.95,4 4,12.95 4,24 C4,35.05 12.95,44 24,44 C35.05,44 44,35.05 44,24 C44,19.3 42.5809627,15.1645919 39.7428882,11.5937758" transform="translate(-2.000000, -2.000000)"></path>
+                    </svg>
+                    </div>
+                </label>
+                    <span className="todoText">{' '}
+                    { title }</span>
                  </div>
                 <div className="delBtn" onClick={this.props.delTodo.bind(this, id)}>
                 </div>
